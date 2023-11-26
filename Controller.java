040b6072676200e28980f7487d5b4559334f9131
Szalami72 <145064120+Szalami72@users.noreply.github.com>
2023-11-26 22:23:08 +0100

    Add files via upload

---
+++ Controller.java
@@ -0,0 +1,34 @@
+/*
+* File: Controller.java
+* Author: Faragó Csaba
+* Copyright: 2023, Faragó Csaba
+* Group: Szoft II_1_E
+* Date: 2023-11-26
+* Github: https://github.com/janos/
+* Licenc: GNU GPL
+*/
+public class Controller implements Controllable {
+    public static void main(String[] args) throws Exception {
+        Controller controller = new Controller();
+        controller.start();
+        controller.stop();
+        controller.restart();
+    }
+
+    @Override
+    public void start() {
+        System.out.println("A start metódus elindult!");
+    }
+
+    @Override
+    public void stop() {
+        System.out.println("A stop metódus elindult!");
+
+    }
+
+    @Override
+    public void restart() {
+        System.out.println("A restart metódus elindult!");
+
+    }
+}
